f098db743e50e03659dceb2638728f14693787cf
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-02-25 12:21:34 -0300

    feat: add accounts_view

---
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -0,0 +1,15 @@
+defmodule RocketpayWeb.AccountsView do
+
+  alias Rocketpay.Account
+
+  def render("update.json", %{account: %Account{id: account_id,balance: balance}}) do
+
+    %{
+      message: "Ballance changed successfylly",
+      account: %{
+        id: account_id,
+        balance: balance
+      }
+    }
+  end
+end
