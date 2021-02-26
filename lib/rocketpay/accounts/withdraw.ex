e3d707591640b6074fdf7691ec66151a009f0fa4
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-02-26 17:15:34 -0300

    fifth day <nlw#4>

--- lib/rocketpay/accounts/withdraw.ex
+++ lib/rocketpay/accounts/withdraw.ex
@@ -10,8 +10,11 @@ defmodule Rocketpay.Accounts.Withdraw do
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
-      {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{account_withdraw: account}} -> {:ok, account}
+      {:error, _operation, reason, _changes} ->
+        {:error, reason}
+
+      {:ok, %{withdraw: account}} ->
+        {:ok, account}
     end
-   end
+  end
 end
